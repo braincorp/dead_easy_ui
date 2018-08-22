8ff42337d0d76d758dd3c0f8e042aaee5c9e6dcc
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2018-08-22 08:53:26 -0700

    running tests

--- setup.py
+++ setup.py
@@ -17,5 +17,19 @@ setup(
     version='0.0.1',
     scripts=[],
     packages=[],
-    install_requires=[]
+    install_requires=[
+        'pylint==1.9.2',
+        'astroid==1.6.5',  # this is a pylint dependency. It is very sensitive to this version because of constant bugs.
+        'py==1.5.2',
+        'pep8==1.7.1',
+        'pytest==3.6.3',
+        'pytest-cache==1.0',
+        'pytest-pep8==1.0.6',
+        'pytest-cov==2.5.1',
+        'pytest-xdist==1.19.1',
+        'pytest-ordering==0.5',
+        'pytest-pylint==0.11.0',
+        'pytest-timeout==1.2.1',
+        'future==0.16.0',  # python 3 compatibility
+    ]
 )
