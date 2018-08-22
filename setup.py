60e9809eb7e470751b7445712dca48fed3eb8193
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2018-08-22 08:47:15 -0700

    setting up

---
+++ setup.py
@@ -0,0 +1,21 @@
+# ============================================================================
+# Copyright 2017 BRAIN Corporation. All rights reserved. This software is
+# provided to you under BRAIN Corporation's Beta License Agreement and
+# your use of the software is governed by the terms of that Beta License
+# Agreement, found at http://www.braincorporation.com/betalicense.
+# ============================================================================
+
+from setuptools import setup
+
+
+setup(
+    name='dead-easy-ui',
+    author='Brain Corporation',
+    author_email='peter.ed.oconnor@gmail.com',
+    url='https://github.com/braincorp/dead_easy_ui',
+    long_description='A package for streamline creation of console ui',
+    version='0.0.1',
+    scripts=[],
+    packages=[],
+    install_requires=[]
+)
